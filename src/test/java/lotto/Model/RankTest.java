6bfe7e0947d9cafd05515dc8ebd9c1289cecd3a1
Lee Sruim <sssurim@naver.com>
2025-10-31 22:41:30 +0900

    전체 파일 다시 업로드(RankTest)

--- src/test/java/lotto/Model/RankTest.java
+++ src/test/java/lotto/Model/RankTest.java
@@ -1,4 +1,37 @@
 package lotto.Model;
 
-public class Rank {
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RankTest {
+
+    @Test
+    void 일치6개면_1등() {
+        assertThat(Rank.getRank(6, false)).isEqualTo(Rank.First);
+    }
+
+    @Test
+    void 일치5개_보너스일치면_2등() {
+        assertThat(Rank.getRank(5, true)).isEqualTo(Rank.Second);
+    }
+
+    @Test
+    void 일치5개면_3등() {
+        assertThat(Rank.getRank(5, false)).isEqualTo(Rank.Third);
+    }
+
+    @Test
+    void 일치4개면_4등() {
+        assertThat(Rank.getRank(4, false)).isEqualTo(Rank.Fourth);
+    }
+
+    @Test
+    void 일치3개면_5등() {
+        assertThat(Rank.getRank(3, false)).isEqualTo(Rank.Fifth);
+    }
+
+    @Test
+    void 나머지는_꽝() {
+        assertThat(Rank.getRank(2, false)).isEqualTo(Rank.None);
+    }
 }
