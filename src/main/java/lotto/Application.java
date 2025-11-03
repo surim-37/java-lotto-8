0b7916ce2921640c998280b7b995e8cd7dd08ad5
Lee Sruim <sssurim@naver.com>
2025-11-03 18:23:44 +0900

    merge: reconnect history with upstream

---
+++ src/main/java/lotto/Application.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import lotto.Controller.LottoController;
+
+public class Application {
+    public static void main(String[] args) {
+        LottoController controller = new LottoController();
+        controller.run();
+    }
+}
+
